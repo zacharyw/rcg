6b8f86f779692520e30a877635fa6cf93b264037
Zachary <zacharygwright@gmail.com>
2017-02-12 09:52:04 -0500

    Add Twitter Bootstrap

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module Rcg
   class Application < Rails::Application
+
+    config.web_console.whitelisted_ips = '10.0.2.2'
+    config.web_console.development_only = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
