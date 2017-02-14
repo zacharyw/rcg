7c4491ba337abb0fbecb1ddaff4c4d9f1fc7ec6f
Zachary <zacharygwright@gmail.com>
2017-02-13 22:21:29 -0500

    Add ability to login with either username or email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,18 @@ class User < ApplicationRecord
 
   has_many :conversations
   has_many :messages
+
+  attr_accessor :login
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false}
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      conditions[:email].downcase! if conditions[:email]
+      where(conditions.to_h).first
+    end
+  end
 end
