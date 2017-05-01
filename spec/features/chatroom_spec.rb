d456e005b3a7d27adf9cbf48b63ecb79753d843d
Zachary <zacharygwright@gmail.com>
2017-04-30 20:26:11 -0400

    Feature spec for chatroom.

---
+++ spec/features/chatroom_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature "Chatroom", js: true do
+  let!(:conversation) { FactoryGirl.create(:conversation) }
+  let(:serialized_conversation) { ConversationSerializer.new(conversation) }
+  let(:user) { FactoryGirl.create(:user) }
+
+  scenario 'User enters chatroom and sends a message' do
+    log_in_as(user)
+    
+    visit conversations_path
+
+    click_link serialized_conversation.preview
+
+    expect(page).to have_content conversation.messages.first.body
+
+    fill_in 'message_body', with: 'New message'
+    click_on 'Send'
+
+    expect(page).to have_content "#{user.username}: New message"
+  end
+end
