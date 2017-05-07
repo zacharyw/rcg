040146e95562e4a298b5811d2bc3ae177cf0db8e
Zachary <zacharygwright@gmail.com>
2017-05-07 13:11:38 -0400

    Fix redis.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,5 @@
 $redis = nil
+
 if Rails.env.production?
-  $redis = Redis.new(url: "redis://h:pfde2ad96948dc464ca14d1ff22f7affb20e400abfdd6fe9228223b01e5cb0876@ec2-34-206-162-178.compute-1.amazonaws.com:34819")
+  Redis.new(url: ENV["REDIS_URL"])
 end
