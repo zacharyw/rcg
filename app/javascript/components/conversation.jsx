6db4a1a360b301b6dbe1472fd0c91a91603e1271
Zachary <zacharygwright@gmail.com>
2017-06-03 11:38:19 -0400

    Adjust gutter width on small screens.

--- app/javascript/components/conversation.jsx
+++ app/javascript/components/conversation.jsx
@@ -8,7 +8,7 @@ class Conversation extends React.Component {
   render() {
     const conversation = this.props.conversation;
     return (
-      <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+      <div className="conversation-col col-xs-12 col-sm-6 col-md-4 col-lg-3">
         <div className="panel panel-default conversation" id={"conversation_" + conversation.id}>
           <div className="panel-heading">
             {conversation.author} <span className="text-muted">{conversation.time_ago} ago</span>
