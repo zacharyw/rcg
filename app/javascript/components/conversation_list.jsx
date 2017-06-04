c666693412d6fc87d20c4936cf45cc73eeaf6e82
Zachary <zacharygwright@gmail.com>
2017-06-04 08:24:45 -0400

    Code cleanup.

--- app/javascript/components/conversation_list.jsx
+++ app/javascript/components/conversation_list.jsx
@@ -25,8 +25,8 @@ class ConversationList extends React.Component {
     return (
       <div id="conversation-list">
         { this.props.new_conversation_form.length > 0 &&
-            <div className="conversation-col col-xs-12 col-sm-6 col-md-4 col-lg-3" dangerouslySetInnerHTML={{__html: this.props.new_conversation_form}}>
-            </div>
+          <div className="conversation-col col-xs-12 col-sm-6 col-md-4 col-lg-3" dangerouslySetInnerHTML={{__html: this.props.new_conversation_form}}>
+          </div>
         }
         {conversationList}
       </div>
